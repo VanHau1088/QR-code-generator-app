d2c477a1b5a09d1e09d282cbc4c047a7a5354f7a
Van Hau <hn4901298@gmail.com>
2025-02-17 17:09:20 +0700

    f

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './src', // Đặt thư mục gốc của dự án (nếu cần)
-  build: {
-    outDir: 'build', // Thư mục đầu ra của build
-  },
 })
